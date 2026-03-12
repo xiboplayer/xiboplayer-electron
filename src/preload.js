2b5e0f4cad4beaedd099a58c825f9f058aa16443
Pau Aliagas <linuxnow@gmail.com>
2026-03-12 13:32:02 +0000

    chore: add AGPL-3.0-or-later license and SPDX headers

--- src/preload.js
+++ src/preload.js
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (c) 2024-2026 Pau Aliagas <linuxnow@gmail.com>
+
 /**
  * Preload script - Security bridge between main and renderer process
  *
